5be2f16515b5fc04e1c578cd897d380418c78463
abdullayev23 <muhammadqodir.org@gmail.com>
2023-09-24 11:46:48 +0500

    Js code

---
+++ js/main.js
@@ -0,0 +1,55 @@
+let hamburger = document.querySelector(".buton");
+
+hamburger.onclick = function() {
+    let elUl2 = document.querySelector(".buton");
+    elUl2.classList.toggle("active");
+}
+
+let hamburger2 = document.querySelector(".buton2");
+
+hamburger2.onclick = function() {
+    let elUl2 = document.querySelector(".buton2");
+    elUl2.classList.toggle("active");
+}
+
+let hamburger3 = document.querySelector(".buton3");
+
+hamburger3.onclick = function() {
+    let elUl3 = document.querySelector(".buton3");
+    elUl3.classList.toggle("active");
+}
+
+let hamburger5 = document.querySelector(".buton5");
+
+hamburger5.onclick = function() {
+    let elUl5 = document.querySelector(".buton5");
+    elUl5.classList.toggle("active");
+}
+
+let hamburger6 = document.querySelector(".buton6");
+
+hamburger6.onclick = function() {
+    let elUl6 = document.querySelector(".buton6");
+    elUl6.classList.toggle("active");
+}
+
+let hamburger7 = document.querySelector(".buton7");
+
+hamburger7.onclick = function() {
+    let elUl7 = document.querySelector(".buton7");
+    elUl7.classList.toggle("active");
+}
+
+let hamburger8 = document.querySelector(".btn-head");
+
+hamburger8.onclick = function() {
+    let elUl8 = document.querySelector(".list");
+    elUl8.classList.toggle("active");
+}
+
+let hamburger9 = document.querySelector(".head-btn");
+
+hamburger9.onclick = function() {
+    let elUl9 = document.querySelector(".list2");
+    elUl9.classList.toggle("active");
+}
